7a6b1356bf1674d1b678131ab7e99f3f1268c6ad
Boum <boume13@hotmail.com>
2026-04-02 23:02:43 +0100

    feat: frontend boilerplate - react, tailwind, routing setup

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import HomePage from './pages/HomePage';
+import AuthPage from './pages/AuthPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth" element={<AuthPage />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
